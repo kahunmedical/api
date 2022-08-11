63e004cb9d303052b9a32a80ed32828a5d8d0816
De-Gul <gilad@kahun.com>
2022-08-11 18:20:37 +0300

    example page

--- examples/html/js/script.js
+++ examples/html/js/script.js
@@ -1,11 +1,13 @@
 let kahunCaseRecord;
+let clinicalData = null;
 let widgetSettings = {};
+let overrides = null;
 
 const defaultJs = `let kahunCaseRecord;
 async function onKahunLoaded() {
    kahunCaseRecord = await Kahun.newCaseRecord({
-      apiKey: "Your API Key",
-      clinicId: "Your Clinic Id",{caseProps}
+      apiKey: "&lt;provided api key&gt",
+      clinicId: "&lt;provided clinic id&gt",{caseProps}
    });
 }
 async function startKahunChat() {
@@ -14,32 +16,44 @@ async function startKahunChat() {
 const defaultHtml = `&lt;script src="https://patient.kahun.com/api/clientapi.js" onload="onKahunLoaded()" async&gt;&lt;/script&gt;
 &lt;div id="kahun-patient"{windowAlign}&gt;&lt;/div&gt;
 &lt;button class="kahun-button" onclick="startKahunChat()"&gt;Start Chat&lt;/button&gt;`
-async function onKahunLoaded() {
+const demoInitialData = `{
+  "caseFormat": "athena",
+  "case": {
+    "demographics": {
+      "age": 54,
+      "gender": "M"
+    },
+    "problems": [
+      {
+        "lastmodifieddatetime": "2022-04-14T02:19:14-04:00",
+        "name": "Renal disorder",
+        "codeset": "SNOMED",
+        "code": "420279001"
+      }
+    ]
+  }
+}`
 
+async function onKahunLoaded() {
     kahunCaseRecord = await Kahun.newCaseRecord({
         apiKey: "DuISmyrBbuac2EjGTAxYgatyQTPDKmM53sYbxt3M",
-        clinicId: "74e8a537-d812-4f36-9ff2-c724a223d3e0",
-        // clinicalData: {
-        //     "caseFormat": "athena",
-        //     "case": {
-        //         "demographics": {
-        //             "age": 54,
-        //             "gender": "M"
-        //         },
-        //     }
-        // }
+        clinicId: "799a2904-e59e-4ffe-8747-a762250c7a93",
+        clinicalData,
+        "overrides":overrides,
     });
     console.log("The new case id is", kahunCaseRecord.getCaseId())
-
 }
 async function startKahunChat() {
     await kahunCaseRecord.startChatBotWidget(widgetSettings);
 }
 
 function generateCode() {
-    let caseProps = null;
-    let windowAlign = null;
+    let caseProps = "";
+    let windowAlign = "";
     let widgetSettingsCopy = {};
+    clinicalData = null;
+    overrides = {};
+    widgetSettings = {};
     const radioButtons = document.querySelectorAll("input[name='windowAlign']")
     for (const radioButton of radioButtons) {
         if (radioButton.checked) {
@@ -47,34 +61,76 @@ function generateCode() {
             break;
         }
     }
-    if (windowAlign) {
-        document.querySelector('#kahun-patient').setAttribute("data-window-alignment", windowAlign);
-        onKahunLoaded();
-    }
-    const locale = document.querySelector('#locale').value;
+    if (windowAlign) document.querySelector('#kahun-patient').setAttribute("data-window-alignment", windowAlign);
+
+    const locale = stringValue('#locale');
     if (locale) {
-        widgetSettings.locale = locale;
         widgetSettingsCopy.locale = locale;
     }
-    const utmString = document.querySelector('#utmString').value;
+    const utmString = stringValue('#utmString');
     if (utmString) {
-        widgetSettings.utmString = utmString;
         widgetSettingsCopy.utmString = utmString;
     }
-    const onCompletionUrl = document.querySelector('#onCompletionUrl').value;
+    const onCompletionUrl = stringValue('#onCompletionUrl');
     if (onCompletionUrl) {
-        widgetSettings.onCompletionUrl = onCompletionUrl;
         widgetSettingsCopy.onCompletionUrl = onCompletionUrl;
     }
-    const onAbandonUrl = document.querySelector('#onAbandonUrl').value;
+    const onAbandonUrl = stringValue('#onAbandonUrl');
     if (onAbandonUrl) {
-        widgetSettings.onAbandonUrl = onAbandonUrl;
         widgetSettingsCopy.onAbandonUrl = onAbandonUrl;
+    }
+    const initialData = stringValue('#initialData');
+    if (initialData) {
+        //let parsedInitialData = JSON.parse(initialData);
+        clinicalData = JSON.parse(initialData);
+        caseProps = "\nclinicalData:" + initialData;
+    }
+    const welcomePatient = stringValue('#welcomePatient');
+    if (welcomePatient) {
+        overrides.WELCOME_PATIENT = welcomePatient
+    }
+    if (Object.keys(overrides).length > 0) caseProps += (caseProps?",":"") + "\n\"overrides\":" + JSON.stringify(overrides)
+
+    let widgetSettingsStr = '';
+    if (widgetSettingsCopy && Object.keys(widgetSettingsCopy).length>0) {
+        widgetSettingsStr = JSON.stringify(widgetSettingsCopy).replace('{','\n')
+        widgetSettings = {
+            ...widgetSettings,
+            ...widgetSettingsCopy
+        }
     }
 
     document.querySelector('#htmlCode').innerHTML = defaultHtml.replace('{windowAlign}',windowAlign?' data-window-alignment="' + windowAlign + '"':'');
-    document.querySelector('#jsCode').innerHTML = defaultJs.replace('{caseProps}',caseProps?caseProps:'').replace('{widgetSettings}',widgetSettings && Object.keys(widgetSettings).length>0?JSON.stringify(widgetSettingsCopy):'');
+    document.querySelector('#jsCode').innerHTML = defaultJs.replace('{caseProps}',caseProps?caseProps:'').replace('{widgetSettings}',widgetSettingsStr);
     Prism.highlightAll();
+
+    onKahunLoaded();
+    reloadButton();
+}
+
+function toggleSection(e,objId) {
+    e.classList.toggle('open');
+    document.querySelector(objId).classList.toggle('open');
+}
+
+function resetCode() {
+    document.querySelector('#genForm').reset();
+    document.querySelector('#kahun-patient').removeAttribute("data-window-alignment");
+    generateCode();
+}
+
+function reloadButton() {
+    const button = document.querySelector('#kahunButton');
+    var newone = button.cloneNode(true);
+    button.parentNode.replaceChild(newone, button);
+}
+
+function stringValue(selector) {
+    return document.querySelector(selector).value.trim();
+}
+
+function addDemoData() {
+    document.querySelector('#initialData').value = demoInitialData;
 }
 
 window.onload = () => {
