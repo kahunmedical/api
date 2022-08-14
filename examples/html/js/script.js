0ec0d3f9c9f6a0e32671792bc5af6c7700abae54
De-Gul <gilad@kahun.com>
2022-08-14 16:17:19 +0300

    example page

--- examples/html/js/script.js
+++ examples/html/js/script.js
@@ -1,6 +1,6 @@
 let kahunCaseRecord;
 let clinicalData = null;
-let widgetSettings = {};
+let widgetSettings1 = {locale:"he"};
 let overrides = null;
 
 const defaultJs = `let kahunCaseRecord;
@@ -41,19 +41,30 @@ async function onKahunLoaded() {
         clinicalData,
         "overrides":overrides,
     });
-    console.log("The new case id is", kahunCaseRecord.getCaseId())
+    printToStatus("<strong>Kanun loaded</strong><br/>The new case id is " + kahunCaseRecord.getCaseId())
+    kahunCaseRecord.on("widget_state", (evt) => {
+        printToStatus(`<i>Widget State:</i> ${evt.state}`);
+    })
+    kahunCaseRecord.on("status", (evt) => {
+        printToStatus(`<i>Progress:</i> ${evt.progress}%`);
+    })
+    kahunCaseRecord.on("done", (evt) => {
+        printToStatus("<strong>User has completed the chat</strong>");
+    })
 }
 async function startKahunChat() {
-    await kahunCaseRecord.startChatBotWidget(widgetSettings);
+    toggleCloseButton(true);
+    await kahunCaseRecord.startChatBotWidget(widgetSettings1);
 }
 
 function generateCode() {
+    clearStatus();
     let caseProps = "";
     let windowAlign = "";
     let widgetSettingsCopy = {};
     clinicalData = null;
     overrides = {};
-    widgetSettings = {};
+    widgetSettings1 = {};
     const radioButtons = document.querySelectorAll("input[name='windowAlign']")
     for (const radioButton of radioButtons) {
         if (radioButton.checked) {
@@ -89,13 +100,25 @@ function generateCode() {
     if (welcomePatient) {
         overrides.WELCOME_PATIENT = welcomePatient
     }
+    const welcomePartners = stringValue('#welcomePartners');
+    if (welcomePartners) {
+        overrides.WELCOME_PARTNERS = welcomePartners
+    }
+    const thankYouPatient = stringValue('#thankYouPatient');
+    if (thankYouPatient) {
+        overrides.THANK_YOU_PATIENT = thankYouPatient
+    }
+    const thanksSubTitle = stringValue('#thanksSubTitle');
+    if (thanksSubTitle) {
+        overrides.THANKS_SUB_TITLE = thanksSubTitle
+    }
     if (Object.keys(overrides).length > 0) caseProps += (caseProps?",":"") + "\n\"overrides\":" + JSON.stringify(overrides)
 
     let widgetSettingsStr = '';
     if (widgetSettingsCopy && Object.keys(widgetSettingsCopy).length>0) {
-        widgetSettingsStr = JSON.stringify(widgetSettingsCopy).replace('{','\n')
-        widgetSettings = {
-            ...widgetSettings,
+        widgetSettingsStr = JSON.stringify(widgetSettingsCopy)
+        widgetSettings1 = {
+            ...widgetSettings1,
             ...widgetSettingsCopy
         }
     }
@@ -121,7 +144,7 @@ function resetCode() {
 
 function reloadButton() {
     const button = document.querySelector('#kahunButton');
-    var newone = button.cloneNode(true);
+    let newone = button.cloneNode(true);
     button.parentNode.replaceChild(newone, button);
 }
 
@@ -131,6 +154,23 @@ function stringValue(selector) {
 
 function addDemoData() {
     document.querySelector('#initialData').value = demoInitialData;
+}
+
+function printToStatus(txt) {
+    document.querySelector('#terminal').innerHTML +=  txt +'<br/>';
+}
+
+function clearStatus() {
+    document.querySelector('#terminal').innerHTML = '';
+}
+
+function toggleCloseButton(on) {
+    document.querySelector('#closeWidget').style.display = on? 'block':'none';
+}
+
+function closeWidget() {
+    toggleCloseButton(false);
+    kahunCaseRecord.removeChatBotWidget();
 }
 
 window.onload = () => {
